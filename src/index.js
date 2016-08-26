0569fd1157062d00b80c23f33bbe263a478ff112
tgrander <treygranderson@gmail.com>
2016-08-26 12:48:47 -0400

    Revert "[feat] Redux Dev Tools"
    This reverts commit bc1bf2d798f3fe909539c42f2ce3ae631ae1a52b.

--- src/index.js
+++ src/index.js
@@ -5,14 +5,12 @@ import { Provider } from 'react-redux'
 import configureStore from './store/store'
 
 import App from './App';
-import DevTools from './containers/DevTools';
 
 const store = configureStore()
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
-    <DevTools />
   </Provider>,
   document.getElementById('root')
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,18 +1,13 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-
 import roommaideApp from '../reducers/index'
-import DevTools from '../containers/DevTools';
 
 export default function configureStore(preloadedState){
   return createStore(
     roommaideApp,
     preloadedState,
-    compose(
-      applyMiddleware(thunk),
-      window.devToolsExtension ? window.devToolsExtension() : f => f
+    applyMiddleware(
+      thunk
     )
-
-
   )
 }
