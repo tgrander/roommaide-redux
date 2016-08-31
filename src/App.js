723f2bd329be47fd693d247b177411cefa649f1b
tgrander <treygranderson@gmail.com>
2016-08-31 19:37:40 -0400

    Revert "Revert "[feat] style""
    This reverts commit a826b771b0ec5d5d0b840ae5776191438f03d659.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Header from './components/Header'
+
 import RoommateApp from './containers/RoommateApp'
 
 import './css/App.css'
@@ -9,7 +9,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
         <RoommateApp />
       </div>
     );
