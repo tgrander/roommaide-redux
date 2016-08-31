3198d5e269e31a1ccffac70c8a5dc98423c2a3a7
tgrander <treygranderson@gmail.com>
2016-08-31 15:44:31 -0400

    [feat] Responsive logo shadow

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
+
+import { styleLogo } from '../utilities/styleFunctions'
 
 import '../css/Header.css'
 
-const Header = () => (
-  <div className='header'>
-    <h1 className='logo'>ROOMMAIDE</h1>
-  </div>
-)
+// const Header = () => (
+//
+// )
+
+class Header extends Component {
+
+  componentDidMount(){
+    styleLogo("white")
+  }
+
+  render(){
+    console.log('STYLE LOGO FUNCTION: ', styleLogo);
+    return (
+      <div className='header'>
+        <h1 className='logo' id='title'>ROOMMAIDE</h1>
+      </div>
+    )
+  }
+}
 
 export default Header
