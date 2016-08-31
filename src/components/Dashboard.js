bf00872afc4a40e984ffc0b0adf0faaed6eb25b4
tgrander <treygranderson@gmail.com>
2016-08-31 17:26:43 -0400

    [feat] style

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,6 +4,7 @@ import getTotalPerRoommate from '../reducers/reducer_roommates'
 
 import RoommatesList from './RoommatesList'
 import Summary from './Summary'
+import Header from './Header'
 
 import '../css/Dashboard.css'
 
@@ -19,6 +20,10 @@ const Dashboard = ({
   console.log('per roommate function: ', getTotalPerRoommate);
   const totalPerRoommate = getTotalPerRoommate(total, numberofRoommates)
   return (
+    <div>
+    <div>
+      <Header/>
+    </div>
     <div className='dashboard'>
       <RoommatesList
         roommates={roommates}
@@ -33,6 +38,7 @@ const Dashboard = ({
         numberofRoommates={numberofRoommates}
         totalPerRoommate={totalPerRoommate}
         />
+    </div>
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,24 +4,18 @@ import { styleLogo } from '../utilities/styleFunctions'
 
 import '../css/Header.css'
 
-// const Header = () => (
-//
-// )
-
 class Header extends Component {
 
-  componentDidMount(){
-    styleLogo("white")
-  }
+    componentDidMount(){
+      styleLogo("white")
+    }
 
-  render(){
-    console.log('STYLE LOGO FUNCTION: ', styleLogo);
-    return (
-      <div className='header'>
-        <h1 className='logo' id='title'>ROOMMAIDE</h1>
-      </div>
-    )
-  }
+    render(){
+      return (
+        <h1 className='title'>ROOMMAIDE</h1>
+      )
+    }
+
 }
 
 export default Header
