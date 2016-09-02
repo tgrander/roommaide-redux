b3bd322af8bef092c61c6a67a1c2e292d0174705
tgrander <treygranderson@gmail.com>
2016-09-02 11:31:21 -0400

    Remove link of sidebar creator;

--- src/components/SidebarNav.js
+++ src/components/SidebarNav.js
@@ -18,7 +18,6 @@ const SidebarNav = () => {
           <a href="#"><li>About</li></a>
           <a href="#"><li>Info</li></a>
           <a href="#"><li>Contact</li></a>
-          <a href="https://erikterwan.com/" target="_blank"><li>Show me more</li></a>
         </ul>
       </div>
     </nav>
